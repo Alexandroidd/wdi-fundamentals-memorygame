c47f9a8d44def72353428eac0c095fe0ca2f78f8
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-12 18:53:13 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
  console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -16,8 +39,10 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 
 var flipCard = function(cardID) {
 
-console.log("User flipped " + cards[cardID]);
-cardsInPlay.push(cards[cardID]);
+console.log("User flipped " + cards[cardID].rank);
+console.log(cards[cardID].cardImage);
+console.log(cards[cardID].suit)
+cardsInPlay.push(cards[cardID].rank);
 checkForMatch();
 
 
